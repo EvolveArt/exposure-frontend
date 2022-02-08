4d33233f1c113eb26886ebf33c42d89f632d99c8
Matteo Georges <matteo.georges74@hotmail.fr>
2022-02-08 13:28:23 +0100

    feat: end landing page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -7,6 +7,8 @@ import ether from "../../assets/imgs/ether.png";
 import mintType from "../../assets/imgs/mintType.png";
 import addIcon from "../../assets/imgs/plus.png";
 import ticon from "../../assets/imgs/t.png";
+import arrow from "../../assets/imgs/arrow.png";
+import arrowContainer from "../../assets/imgs/arrowContainer.png";
 
 const arrival = {
   title: "Touching Strangers",
@@ -30,215 +32,241 @@ const arrivalPage = (arrival: {
   releaseHour: string;
 }) => {
   return (
-    <Flex
-      width={{ base: "90%", lg: "80%" }}
-      minHeight={"90vh"}
-      flexDirection={{ base: "column", md: "row" }}
-      justifyContent="center"
-      alignItems={"center"}
-      paddingTop={"85px"}
-      margin="auto"
-      gridGap={"24px"}
-      zIndex="1"
-      paddingBottom={"100px"}
-    >
+    <>
       <Flex
-        width={{ base: "100%", md: "calc(60% - 12px)", lg: "calc(50% - 12px)" }}
-        height={{ base: "fit-content", md: "80vh" }}
-        paddingTop={{ base: "50px", md: "unset" }}
-        paddingBottom={{ base: "50px", md: "unset" }}
-        justifyContent={{ base: "center", md: "end" }}
-        alignItems="center"
-        position="relative"
-      >
-        <Image src={arrival.image}></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"0"}
-          left="0"
-        ></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"0"}
-          right={{ base: "0", md: "-35px" }}
-        ></Image>
-        <Image
-          display={{ base: "unset", md: "none" }}
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"0"}
-          right="0"
-        ></Image>
-        <Image
-          display={{ base: "unset", md: "none" }}
-          src={ticon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"14px"}
-          right="50%"
-          left="50%"
-        ></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"0"}
-          left="0"
-        ></Image>
-      </Flex>
-      <Flex
-        flexDirection={"column"}
-        width={{ base: "100%", md: "calc(40% - 12px)", lg: "calc(50% - 12px)" }}
-        height={{ base: "fit-content", md: "80vh" }}
+        width={{ base: "90%", lg: "80%" }}
+        minHeight={"80vh"}
+        flexDirection={{ base: "column", md: "row" }}
         justifyContent="center"
-        position="relative"
-        paddingBottom={{ base: "50px", md: "unset" }}
+        alignItems={"center"}
+        paddingTop={"85px"}
+        margin="auto"
+        gridGap={"24px"}
+        zIndex="1"
+        paddingBottom={"100px"}
       >
-        <Image
-          display={{ base: "none", md: "unset" }}
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"0"}
-          right="0px"
-        ></Image>
-        <Image
-          display={{ base: "unset", md: "none" }}
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"0"}
-          left="0px"
-        ></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"0"}
-          right="0px"
-        ></Image>
-        <Image
-          display={{ base: "unset", md: "none" }}
-          src={ticon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          transform={"rotate(180deg)"}
-          bottom={"14px"}
-          right="50%"
-          left="50%"
-        ></Image>
-        <Text
-          fontFamily="Inter"
-          fontStyle="normal"
-          fontWeight="800"
-          fontSize="40px"
-          lineHeight="53px"
-          paddingBottom={"8px"}
+        <Flex
+          width={{
+            base: "100%",
+            md: "calc(60% - 12px)",
+            lg: "calc(50% - 12px)",
+          }}
+          height={{ base: "fit-content", md: "80vh" }}
+          paddingTop={{ base: "50px", md: "unset" }}
+          paddingBottom={{ base: "50px", md: "unset" }}
+          justifyContent={{ base: "center", md: "end" }}
+          alignItems="center"
+          position="relative"
         >
-          {arrival.title}
-        </Text>
-        <Text
-          fontFamily="Inter"
-          fontStyle="normal"
-          fontWeight="normal"
-          fontSize={"16px"}
-          lineHeight="28px"
-          paddingBottom={"58px"}
+          <Image src={arrival.image}></Image>
+          <Image
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            top={"0"}
+            left="0"
+          ></Image>
+          <Image
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            top={"0"}
+            right={{ base: "0", md: "-35px" }}
+          ></Image>
+          <Image
+            display={{ base: "unset", md: "none" }}
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            bottom={"0"}
+            right="0"
+          ></Image>
+          <Image
+            display={{ base: "unset", md: "none" }}
+            src={ticon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            top={"14px"}
+            right="50%"
+            left="50%"
+          ></Image>
+          <Image
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            bottom={"0"}
+            left="0"
+          ></Image>
+        </Flex>
+        <Flex
+          flexDirection={"column"}
+          width={{
+            base: "100%",
+            md: "calc(40% - 12px)",
+            lg: "calc(50% - 12px)",
+          }}
+          height={{ base: "fit-content", md: "80vh" }}
+          justifyContent="center"
+          position="relative"
+          paddingBottom={{ base: "50px", md: "unset" }}
         >
-          By {arrival.author}
-        </Text>
-        <Flex flexDirection={"row"} gridGap="9px">
-          <Image src={mintType} width="29px" height="29px" />
+          <Image
+            display={{ base: "none", md: "unset" }}
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            top={"0"}
+            right="0px"
+          ></Image>
+          <Image
+            display={{ base: "unset", md: "none" }}
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            bottom={"0"}
+            left="0px"
+          ></Image>
+          <Image
+            src={addIcon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            bottom={"0"}
+            right="0px"
+          ></Image>
+          <Image
+            display={{ base: "unset", md: "none" }}
+            src={ticon}
+            opacity="0.1"
+            width={"27px"}
+            position="absolute"
+            transform={"rotate(180deg)"}
+            bottom={"14px"}
+            right="50%"
+            left="50%"
+          ></Image>
+          <Text
+            fontFamily="Inter"
+            fontStyle="normal"
+            fontWeight="800"
+            fontSize="40px"
+            lineHeight="53px"
+            paddingBottom={"8px"}
+          >
+            {arrival.title}
+          </Text>
           <Text
             fontFamily="Inter"
             fontStyle="normal"
             fontWeight="normal"
-            fontSize="16px"
+            fontSize={"16px"}
             lineHeight="28px"
-            paddingBottom={"17px"}
+            paddingBottom={"58px"}
           >
-            {arrival.mintType}
+            By {arrival.author}
           </Text>
-        </Flex>
-        <Flex flexDirection={"row"} gridGap="9px">
-          <Image src={ether} width="29px" height="29px" />
-          <Text
-            fontFamily="Inter"
-            fontStyle="normal"
-            fontWeight="normal"
-            fontSize="16px"
-            lineHeight="28px"
-            paddingBottom={"17px"}
-          >
-            Mint price -{" "}
-            <span style={{ fontWeight: "800" }}>{arrival.mintPrice} ETH</span>
-          </Text>
-        </Flex>
-        <Flex flexDirection={"row"} gridGap="9px">
-          <Image src={available} width="29px" height="29px" />
-          <Text
-            fontFamily="Inter"
-            fontStyle="normal"
-            fontWeight="normal"
-            fontSize="16px"
-            lineHeight="28px"
-            paddingBottom={"17px"}
-          >
-            Mint available -{" "}
-            <span style={{ fontWeight: "800" }}>
-              0 / {arrival.mintAvailable}
-            </span>
-          </Text>
-        </Flex>
-        <Flex flexDirection={"row"} gridGap="9px">
-          <Image src={agenda} width="29px" height="29px" />
-          <Text
-            fontFamily="Inter"
-            fontStyle="normal"
-            fontWeight="normal"
-            fontSize="16px"
-            lineHeight="28px"
-            paddingBottom={"35px"}
-          >
-            Release date -{" "}
-            <span style={{ fontWeight: "800" }}>{arrival.releaseDate}</span> at{" "}
-            <span style={{ fontWeight: "800" }}>{arrival.releaseHour} GMT</span>
-          </Text>
-        </Flex>
-        <Flex border={"1px solid #000"} width="100%">
-          <Button
-            fontSize={"sm"}
-            fontWeight={600}
-            fontFamily={"Inter"}
-            color={"white"}
-            bg={"#000"}
-            borderRadius="0px"
-            width={"100%"}
-            height={"64px"}
-            style={{ marginInlineStart: "unset" }}
-            _hover={{
-              transform: "translate3d(4px,4px,0px)",
-            }}
-          >
-            Connect Wallet
-          </Button>
+          <Flex flexDirection={"row"} gridGap="9px">
+            <Image src={mintType} width="29px" height="29px" />
+            <Text
+              fontFamily="Inter"
+              fontStyle="normal"
+              fontWeight="normal"
+              fontSize="16px"
+              lineHeight="28px"
+              paddingBottom={"17px"}
+            >
+              {arrival.mintType}
+            </Text>
+          </Flex>
+          <Flex flexDirection={"row"} gridGap="9px">
+            <Image src={ether} width="29px" height="29px" />
+            <Text
+              fontFamily="Inter"
+              fontStyle="normal"
+              fontWeight="normal"
+              fontSize="16px"
+              lineHeight="28px"
+              paddingBottom={"17px"}
+            >
+              Mint price -{" "}
+              <span style={{ fontWeight: "800" }}>{arrival.mintPrice} ETH</span>
+            </Text>
+          </Flex>
+          <Flex flexDirection={"row"} gridGap="9px">
+            <Image src={available} width="29px" height="29px" />
+            <Text
+              fontFamily="Inter"
+              fontStyle="normal"
+              fontWeight="normal"
+              fontSize="16px"
+              lineHeight="28px"
+              paddingBottom={"17px"}
+            >
+              Mint available -{" "}
+              <span style={{ fontWeight: "800" }}>
+                0 / {arrival.mintAvailable}
+              </span>
+            </Text>
+          </Flex>
+          <Flex flexDirection={"row"} gridGap="9px">
+            <Image src={agenda} width="29px" height="29px" />
+            <Text
+              fontFamily="Inter"
+              fontStyle="normal"
+              fontWeight="normal"
+              fontSize="16px"
+              lineHeight="28px"
+              paddingBottom={"35px"}
+            >
+              Release date -{" "}
+              <span style={{ fontWeight: "800" }}>{arrival.releaseDate}</span>{" "}
+              at{" "}
+              <span style={{ fontWeight: "800" }}>
+                {arrival.releaseHour} GMT
+              </span>
+            </Text>
+          </Flex>
+          <Flex border={"1px solid #000"} width="100%">
+            <Button
+              fontSize={"sm"}
+              fontWeight={600}
+              fontFamily={"Inter"}
+              color={"white"}
+              bg={"#000"}
+              borderRadius="0px"
+              width={"100%"}
+              height={"64px"}
+              style={{ marginInlineStart: "unset" }}
+              _hover={{
+                transform: "translate3d(4px,4px,0px)",
+              }}
+            >
+              Connect Wallet
+            </Button>
+          </Flex>
         </Flex>
       </Flex>
-    </Flex>
+      <Flex
+        justifyContent={"center"}
+        alignItems="center"
+        transform={"translateY(-50px)"}
+        position="relative"
+      >
+        <Image
+          src={arrowContainer}
+          position="absolute"
+          display={{ base: "none", md: "unset" }}
+        />
+        <Image src={arrow} display={{ base: "none", md: "unset" }} />
+      </Flex>
+    </>
   );
 };
 
