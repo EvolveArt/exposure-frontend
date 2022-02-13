78a2cbc5dc73811935e1971a2aaa0b24d663a21d
Matteo Georges <matteo.georges74@hotmail.fr>
2022-02-13 10:32:20 +0100

    feat: modal

--- src/components/ConnectWalletModal/index.tsx
+++ src/components/ConnectWalletModal/index.tsx
@@ -7,107 +7,110 @@ import usePrevious from "hooks/usePrevious";
 
 import Modal from "../Modal";
 import styles from "./styles.module.scss";
+import { Flex } from "@chakra-ui/react";
 
 // eslint-disable-next-line no-undef
 const isMainnet = process.env.REACT_APP_ENV === "MAINNET";
 
 const Option = ({ onClick = null, header, icon, active = false }: any) => {
-	return (
-		<div
-			onClick={onClick}
-			className={cx(styles.option, active && styles.active)}>
-			<div className={styles.header}>{header}</div>
-			<img src={icon} className={styles.icon} alt='option-icon' />
-		</div>
-	);
+  return (
+    <Flex border={"2px solid #000"} width="100%" marginBottom={"20px"}>
+      <div
+        onClick={onClick}
+        className={cx(styles.option, active && styles.active)}
+      >
+        <div className={styles.header}>{header}</div>
+        <img src={icon} className={styles.icon} alt="option-icon" />
+      </div>
+    </Flex>
+  );
 };
 
 const ConnectWalletModal = ({ visible, onClose }: any) => {
-	const { activate, active, connector, error, deactivate } = useWeb3React();
+  const { activate, active, connector, error, deactivate } = useWeb3React();
 
-	// close modal when a connection is successful
-	const activePrevious = usePrevious(active);
-	const connectorPrevious = usePrevious(connector);
-	useEffect(() => {
-		if (
-			visible &&
-			((active && !activePrevious) ||
-				(connector && connector !== connectorPrevious && !error))
-		) {
-			onClose();
-		}
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [active, error, connector, visible, activePrevious, connectorPrevious]);
+  // close modal when a connection is successful
+  const activePrevious = usePrevious(active);
+  const connectorPrevious = usePrevious(connector);
+  useEffect(() => {
+    if (
+      visible &&
+      ((active && !activePrevious) ||
+        (connector && connector !== connectorPrevious && !error))
+    ) {
+      onClose();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [active, error, connector, visible, activePrevious, connectorPrevious]);
 
-	const tryActivation = async (connector: any) => {
-		let conn = typeof connector === "function" ? await connector() : connector;
+  const tryActivation = async (connector: any) => {
+    let conn = typeof connector === "function" ? await connector() : connector;
 
-		Object.keys(SUPPORTED_WALLETS).map((key) => {
-			if (connector === SUPPORTED_WALLETS[key].connector) {
-				return SUPPORTED_WALLETS[key].name;
-			}
-			return true;
-		});
+    Object.keys(SUPPORTED_WALLETS).map((key) => {
+      if (connector === SUPPORTED_WALLETS[key].connector) {
+        return SUPPORTED_WALLETS[key].name;
+      }
+      return true;
+    });
 
-		conn &&
-			activate(conn, undefined, true).catch((error) => {
-				console.log(error);
-				if (error instanceof UnsupportedChainIdError) {
-					activate(conn); // a little janky...can't use setError because the connector isn't set
-				}
-			});
-	};
+    conn &&
+      activate(conn, undefined, true).catch((error) => {
+        console.log(error);
+        if (error instanceof UnsupportedChainIdError) {
+          activate(conn); // a little janky...can't use setError because the connector isn't set
+        }
+      });
+  };
 
-	const getOptions = () => {
-		return Object.keys(SUPPORTED_WALLETS).map((key) => {
-			const option: any = SUPPORTED_WALLETS[key];
+  const getOptions = () => {
+    return Object.keys(SUPPORTED_WALLETS).map((key) => {
+      const option: any = SUPPORTED_WALLETS[key];
 
-			return (
-				<Option
-					onClick={() => {
-						// eslint-disable-next-line @typescript-eslint/no-unused-expressions
-						option.connector === connector
-							? null
-							: tryActivation(option.connector);
-					}}
-					key={key}
-					active={option.connector === connector}
-					header={option.name}
-					icon={option.icon}
-				/>
-			);
-		});
-	};
+      return (
+        <Option
+          onClick={() => {
+            // eslint-disable-next-line @typescript-eslint/no-unused-expressions
+            option.connector === connector
+              ? null
+              : tryActivation(option.connector);
+          }}
+          key={key}
+          active={option.connector === connector}
+          header={option.name}
+          icon={option.icon}
+        />
+      );
+    });
+  };
 
-	const getModalContent = () => {
-		if (error instanceof UnsupportedChainIdError) {
-			return (
-				<div>
-					<div className={styles.text}>
-						Please connect to {isMainnet ? "Ethereum" : "Rinkeby"}.
-					</div>
-					<div className={styles.switchBtn} onClick={deactivate}>
-						Disconnect
-					</div>
-				</div>
-			);
-		}
-		return getOptions();
-	};
+  const getModalContent = () => {
+    if (error instanceof UnsupportedChainIdError) {
+      return (
+        <div>
+          <div className={styles.text}>
+            Please connect to {isMainnet ? "Ethereum" : "Rinkeby"}.
+          </div>
+          <div className={styles.switchBtn} onClick={deactivate}>
+            Disconnect
+          </div>
+        </div>
+      );
+    }
+    return getOptions();
+  };
 
-	return (
-		<Modal
-			visible={visible}
-			title={
-				error instanceof UnsupportedChainIdError
-					? "Wrong Network"
-					: "Connect to a wallet"
-			}
-			onClose={onClose}
-			small>
-			{getModalContent()}
-		</Modal>
-	);
+  return (
+    <Modal
+      visible={visible}
+      title={error instanceof UnsupportedChainIdError ? "Wrong Network" : ""}
+      secondTitle={"Let's get started!"}
+      desc={"Connect with one of our available wallet providers."}
+      onClose={onClose}
+      small
+    >
+      {getModalContent()}
+    </Modal>
+  );
 };
 
 export default ConnectWalletModal;
