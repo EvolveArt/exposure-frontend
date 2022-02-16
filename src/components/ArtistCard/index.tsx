5eff92e1e25f6b151220e2823a6870f7b6783f84
Evolve <Artevolve@yahoo.com>
2022-02-16 16:58:13 +0100

    feat: Artists List 🔨

--- src/components/ArtistCard/index.tsx
+++ src/components/ArtistCard/index.tsx
@@ -1,27 +1,30 @@
 import { Flex, Image, Text } from "@chakra-ui/react";
 import styles from "./styles.module.scss";
 import React from "react";
+import { getRandomIPFS } from "utils";
 
 const ArtistCard = (artists: {
-  image: string;
-  name: string;
-  number: string;
+	image: string;
+	name: string;
+	number: string | number;
 }) => {
-  return (
-    <Flex
-      flexDirection={"column"}
-      paddingRight="12px"
-      paddingLeft={"12px"}
-      className={styles.artistComponent}
-    >
-      <Image src={artists.image}></Image>
-      <Text fontWeight="600" fontSize="20px" lineHeight="35px">
-        {artists.name}
-      </Text>
-      <Text fontSize="12px" lineHeight="18px">
-        <span style={{ fontWeight: "bold" }}>{artists.number}</span> Collections
-      </Text>
-    </Flex>
-  );
+	return (
+		<Flex
+			flexDirection={"column"}
+			paddingRight='12px'
+			paddingLeft={"12px"}
+			className={styles.artistComponent}>
+			<Image
+				src={getRandomIPFS(`ipfs://${artists.image}`)}
+				width={240}
+				height={280}></Image>
+			<Text fontWeight='600' fontSize='20px' lineHeight='35px'>
+				{artists.name}
+			</Text>
+			<Text fontSize='12px' lineHeight='18px'>
+				<span style={{ fontWeight: "bold" }}>{artists.number}</span> Collections
+			</Text>
+		</Flex>
+	);
 };
 export default ArtistCard;
--- src/pages/Artists/index.tsx
+++ src/pages/Artists/index.tsx
@@ -1,101 +1,115 @@
 import { Flex, Image } from "@chakra-ui/react";
+import { useApi } from "api";
+import ArtistCard from "components/ArtistCard";
 import Header from "components/Header";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import addIcon from "../../assets/imgs/plus.png";
 import ticon from "../../assets/imgs/t.png";
 
 const Artists = () => {
-  return (
-    <div>
-      <Header />
-      <Flex
-        width={{ base: "90%", lg: "80%" }}
-        minHeight={"80vh"}
-        flexDirection={"column"}
-        paddingTop={"85px"}
-        margin="auto"
-        gridGap={"24px"}
-        zIndex="1"
-        paddingBottom={"100px"}
-        position="relative"
-      >
-        {" "}
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"100px"}
-          left="0"
-        ></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"100px"}
-          right={{ base: "0", md: "-0px" }}
-        ></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"-20px"}
-          right="0"
-        ></Image>
-        <Image
-          display={{ base: "unset", md: "none" }}
-          src={ticon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"114px"}
-          right="50%"
-          left="50%"
-        ></Image>
-        <Image
-          display={{ base: "none", md: "unset" }}
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          top={"114px"}
-          right="50%"
-          left="50%"
-        ></Image>
-        <Image
-          display={{ base: "unset", md: "none" }}
-          src={ticon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          transform={"rotate(180deg)"}
-          bottom={"-6px"}
-          right="50%"
-          left="50%"
-        ></Image>
-        <Image
-          display={{ base: "none", md: "unset" }}
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"-20px"}
-          right="50%"
-          left="50%"
-        ></Image>
-        <Image
-          src={addIcon}
-          opacity="0.1"
-          width={"27px"}
-          position="absolute"
-          bottom={"-20px"}
-          left="0"
-        ></Image>
-      </Flex>
-    </div>
-  );
+	const [artists, setArtists] = useState([]);
+
+	const { getAllArtists } = useApi();
+
+	useEffect(() => {
+		const updateArtists = async () => {
+			const _artists = await getAllArtists();
+			setArtists(_artists.data);
+			console.log(_artists);
+		};
+		updateArtists();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<div>
+			<Header />
+			<Flex
+				width={{ base: "90%", lg: "80%" }}
+				minHeight={"80vh"}
+				flexDirection={"column"}
+				paddingTop={"85px"}
+				margin='auto'
+				gridGap={"24px"}
+				zIndex='1'
+				paddingBottom={"100px"}
+				position='relative'>
+				{" "}
+				<Image
+					src={addIcon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					top={"100px"}
+					left='0'></Image>
+				<Image
+					src={addIcon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					top={"100px"}
+					right={{ base: "0", md: "-0px" }}></Image>
+				<Image
+					src={addIcon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					bottom={"-20px"}
+					right='0'></Image>
+				<Image
+					display={{ base: "unset", md: "none" }}
+					src={ticon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					top={"114px"}
+					right='50%'
+					left='50%'></Image>
+				<Image
+					display={{ base: "none", md: "unset" }}
+					src={addIcon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					top={"114px"}
+					right='50%'
+					left='50%'></Image>
+				<Image
+					display={{ base: "unset", md: "none" }}
+					src={ticon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					transform={"rotate(180deg)"}
+					bottom={"-6px"}
+					right='50%'
+					left='50%'></Image>
+				<Image
+					display={{ base: "none", md: "unset" }}
+					src={addIcon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					bottom={"-20px"}
+					right='50%'
+					left='50%'></Image>
+				<Image
+					src={addIcon}
+					opacity='0.1'
+					width={"27px"}
+					position='absolute'
+					bottom={"-20px"}
+					left='0'></Image>
+				{artists.map((artist: any) => (
+					<ArtistCard
+						image={artist.imageHash}
+						name={artist.firstname + " " + artist.lastname}
+						number={0}
+					/>
+				))}
+			</Flex>
+		</div>
+	);
 };
 
 export default Artists;
