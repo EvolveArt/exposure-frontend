95a946dd19cc456b4fec4cbe9d6c8497e749f1b3
Evolve <Artevolve@yahoo.com>
2022-02-25 12:06:52 +0100

    feat: contracts interaction 📸
    Adds functions to interact with the following contracts :
    - Exposure Main Contract
    - Sale Platform Contract
    next: UI call these functions

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,103 +1,117 @@
-import axios from 'axios';
+import axios from "axios";
 // eslint-disable-next-line
-import { Artist } from 'interfaces';
+import { Artist } from "interfaces";
 
 // eslint-disable-next-line no-undef
-const isMainnet = process.env.REACT_APP_ENV === 'MAINNET';
+const isMainnet = process.env.REACT_APP_ENV === "MAINNET";
 
 export const useApi = () => {
+	const apiUrl = isMainnet
+		? "https://exposure-rest-api.herokuapp.com"
+		: "https://exposure-rest-api.herokuapp.com";
 
-  const apiUrl = isMainnet
-    ? 'https://exposure-rest-api.herokuapp.com'
-    : 'https://exposure-rest-api-testnet.herokuapp.com';
+	const getAuthToken = async (address: string | null | undefined) => {
+		let result = await axios({
+			method: "post",
+			url: `${apiUrl}/auth/getToken`,
+			data: JSON.stringify({ address: address }),
+			headers: { "Content-Type": "application/json" },
+		});
+		if (result.data.status === "success") {
+			let token = result.data.token;
+			return token;
+		}
+		return null;
+	};
 
-  const getAuthToken = async (address: string | null | undefined) => {
-    let result = await axios({
-      method: 'post',
-      url: `${apiUrl}/auth/getToken`,
-      data: JSON.stringify({ address: address }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-    if (result.data.status === 'success') {
-      let token = result.data.token;
-      return token;
-    }
-    return null;
-  };
+	const getAccountDetails = async (authToken: string | null | undefined) => {
+		const res = await axios({
+			method: "get",
+			url: `${apiUrl}/account/getaccountinfo`,
+			headers: {
+				Authorization: `Bearer ${authToken}`,
+			},
+		});
 
-  const getAccountDetails = async (authToken: string | null | undefined) => {
-    const res = await axios({
-      method: 'get',
-      url: `${apiUrl}/account/getaccountinfo`,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    });
+		return res.data;
+	};
 
-    return res.data;
-  };
+	const getNonce = async (
+		address: string,
+		authToken: string | null | undefined
+	) => {
+		const res = await axios({
+			method: "get",
+			url: `${apiUrl}/account/nonce/${address}`,
+			headers: {
+				Authorization: `Bearer ${authToken}`,
+			},
+		});
+		return res.data;
+	};
 
-  const getNonce = async (address: string, authToken: string | null | undefined) => {
-    const res = await axios({
-      method: 'get',
-      url: `${apiUrl}/account/nonce/${address}`,
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-    });
-    return res.data;
-  };
+	const postArtist = async (
+		artist: Artist,
+		authToken: string | null | undefined
+	) => {
+		const res = await axios({
+			method: "POST",
+			url: `${apiUrl}/artist/artistDetails`,
+			data: JSON.stringify(artist),
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: `Bearer ${authToken}`,
+			},
+		});
 
-  const postArtist = async (artist: Artist, authToken: string | null | undefined) => {
-    const res = await axios({
-      method: 'POST',
-      url: `${apiUrl}/artist/artistDetails`,
-      data: JSON.stringify(artist),
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${authToken}`,
-      },
-    })
+		return res.data;
+	};
 
-    return res.data;
-  }
+	const getAllArtists = async () => {
+		const res = await axios({
+			method: "GET",
+			url: `${apiUrl}/artist/fetchAllArtists`,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-  const getAllArtists = async () => {
-    const res = await axios({
-      method: 'GET',
-      url: `${apiUrl}/artist/fetchAllArtists`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+		return res.data;
+	};
 
-    return res.data;
-  }
+	const getAllCollections = async () => {
+		const res = await axios({
+			method: "GET",
+			url: `${apiUrl}/collection/fetchAllCollections`,
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 
-  const getAllCollections = async () => {
-    const res = await axios({
-      method: 'GET',
-      url: `${apiUrl}/collection/fetchAllCollections`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+		return res.data;
+	};
 
-    return res.data;
-  }
+	const getCollectionInfo = async (collectionName: string) => {
+		const res = await axios({
+			method: "POST",
+			url: `${apiUrl}/collection/getCollectionInfo`,
+			headers: {
+				"Content-Type": "application/json",
+			},
+			data: { collectionName },
+		});
 
-  const getCollectionInfo = async (collectionName: string) => {
-    const res = await axios({
-      method: 'POST',
-      url: `${apiUrl}/collection/getCollectionInfo`,
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      data: { collectionName },
-    })
+		return res.data;
+	};
 
-    return res.data;
-  }
-
-  return { getAuthToken, getAccountDetails, postArtist, getNonce, apiUrl, getAllArtists, getAllCollections, getCollectionInfo }
-}
+	return {
+		getAuthToken,
+		getAccountDetails,
+		postArtist,
+		getNonce,
+		apiUrl,
+		getAllArtists,
+		getAllCollections,
+		getCollectionInfo,
+	};
+};
---
+++ src/contracts/exposure.ts
@@ -0,0 +1,83 @@
+import { ChainId } from "@sushiswap/sdk";
+
+import { calculateGasMargin, getHigherGWEI } from "utils";
+import { Contracts } from "constants/networks";
+import useContract from "hooks/useContract";
+
+import { EXPOSURE_MAIN_ABI } from "./abi";
+import { useWeb3React } from "@web3-react/core";
+
+// eslint-disable-next-line no-undef
+const isMainnet = process.env.REACT_APP_ENV === "MAINNET";
+const CHAIN = isMainnet ? ChainId.MAINNET : ChainId.RINKEBY;
+
+export const useExposureContract = () => {
+	const { getContract } = useContract();
+	const { library } = useWeb3React();
+
+	const getExposureContract = async () =>
+		await getContract(Contracts[CHAIN].ExposureMain, EXPOSURE_MAIN_ABI);
+
+	const createDrop = async (artist: string, max: number, from: string) => {
+		const contract = await getExposureContract();
+		const args = [artist, max];
+
+		const options = {
+			from,
+			gasPrice: getHigherGWEI(library),
+			gasLimit: 0,
+		};
+
+		const gasEstimate = await contract.estimateGas[
+			"createDrop(address,uint256)"
+		](...args, options);
+		options.gasLimit = calculateGasMargin(gasEstimate);
+		return await contract.createDrop(...args, options);
+	};
+
+	const setDropIPFS = async (dropId: number, cid: string, from: string) => {
+		const contract = await getExposureContract();
+		const args = [dropId, cid];
+
+		const options = {
+			from,
+			gasPrice: getHigherGWEI(library),
+			gasLimit: 0,
+		};
+
+		const gasEstimate = await contract.estimateGas[
+			"setDropIPFS(uint256,string)"
+		](...args, options);
+		options.gasLimit = calculateGasMargin(gasEstimate);
+		return await contract.setDropIPFS(...args, options);
+	};
+
+	const unpauseDrop = async (
+		dropId: number,
+		shouldUnpause: boolean,
+		from: string
+	) => {
+		const contract = await getExposureContract();
+		const args = [dropId, shouldUnpause];
+
+		const options = {
+			from,
+			gasPrice: getHigherGWEI(library),
+			gasLimit: 0,
+		};
+
+		const gasEstimate = await contract.estimateGas["unpauseDrop(uint256,bool)"](
+			...args,
+			options
+		);
+		options.gasLimit = calculateGasMargin(gasEstimate);
+		return await contract.unpauseDrop(...args, options);
+	};
+
+	return {
+		getExposureContract,
+		createDrop,
+		setDropIPFS,
+		unpauseDrop,
+	};
+};
