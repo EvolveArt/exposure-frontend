d2203fdb5b095c9650ad60d71a381f4a154f0ecd
Matteo Georges <matteo.georges74@hotmail.fr>
2022-03-13 21:06:19 +0100

    feat: add sold out

--- src/components/NFTitem/index.tsx
+++ src/components/NFTitem/index.tsx
@@ -6,57 +6,79 @@ import { useHistory } from "react-router-dom";
 import { formatName, getRandomIPFS } from "utils";
 import styles from "./styles.module.scss";
 
-const NftItem = (collection: Collection) => {
-	const history = useHistory();
+const NftItem = (collection: Collection, isSoldOut: boolean) => {
+  const history = useHistory();
 
-	return (
-		<Flex
-			minHeight={{ base: "410px", lg: "520px" }}
-			width={{ base: "100%", sm: "90%", md: "calc(33.33% - 16px)" }}
-			flexDir={"column"}
-			borderRadius={"2px"}
-			className={styles.dropContainer}
-			onClick={() => history.push(`/collection/${collection.dropId}`)}>
-			<Flex
-				width='100%'
-				position='relative'
-				paddingBottom='100%'
-				boxSizing='border-box'
-				className={styles.imageContainer}>
-				<Image
-					src={getRandomIPFS(`ipfs://${collection?.logoImageHash}`)}
-					position='absolute'
-					top='0'
-					left='0'
-					width='100%'
-					height='100%'
-					backgroundSize='contain'
-					objectFit='contain'
-					border='0'
-					padding='8px'></Image>
-			</Flex>
-			<Flex
-				flexDirection={"column"}
-				paddingLeft='8px'
-				gridGap={"8px"}
-				paddingBottom='8px'>
-				<Text
-					fontStyle='normal'
-					fontWeight='600'
-					fontSize='20px'
-					lineHeight='35px'
-					paddingTop='19px'>
-					{collection.collectionName}
-				</Text>
-				<Text fontWeight='normal' fontSize='14px' lineHeight='28px'>
-					By {collection.artists && formatName(collection.artists[0])}
-				</Text>
-				<Text fontSize='12px' lineHeight='18px'>
-					<span style={{ fontWeight: "bold" }}>{collection.totalSupply} </span>
-					photos
-				</Text>
-			</Flex>
-		</Flex>
-	);
+  return (
+    <Flex
+      minHeight={{ base: "430px", lg: "550px" }}
+      width={{ base: "100%", sm: "90%", md: "calc(33.33% - 16px)" }}
+      flexDir={"column"}
+      borderRadius={"2px"}
+      className={styles.dropContainer}
+      onClick={() => history.push(`/collection/${collection.dropId}`)}
+    >
+      <Flex
+        width="100%"
+        position="relative"
+        paddingBottom="100%"
+        boxSizing="border-box"
+        className={styles.imageContainer}
+      >
+        <Image
+          src={getRandomIPFS(`ipfs://${collection?.logoImageHash}`)}
+          position="absolute"
+          top="0"
+          left="0"
+          width="100%"
+          height="100%"
+          backgroundSize="contain"
+          objectFit="contain"
+          border="0"
+          padding="8px"
+        ></Image>
+      </Flex>
+      <Flex
+        flexDirection={"column"}
+        paddingLeft="8px"
+        gridGap={"8px"}
+        paddingBottom="8px"
+      >
+        <Text
+          fontStyle="normal"
+          fontWeight="600"
+          fontSize="20px"
+          lineHeight="35px"
+          paddingTop="19px"
+        >
+          {collection.collectionName}
+        </Text>
+        <Text fontWeight="normal" fontSize="14px" lineHeight="28px">
+          By {collection.artists && formatName(collection.artists[0])}
+        </Text>
+        <Text fontSize="12px" lineHeight="18px">
+          <span style={{ fontWeight: "bold" }}>{collection.totalSupply} </span>
+          photos
+        </Text>
+      </Flex>
+      {isSoldOut && (
+        <Flex
+          width={"100%"}
+          height="32px"
+          backgroundColor={"#000"}
+          textAlign="center"
+          fontWeight="700"
+          fontSize="16px"
+          lineHeight="20px"
+          color={"#fff"}
+          justifyContent="center"
+          alignItems={"center"}
+          marginTop="auto"
+        >
+          Sold Out
+        </Flex>
+      )}
+    </Flex>
+  );
 };
 export default NftItem;
--- src/pages/ExploreCollection/index.tsx
+++ src/pages/ExploreCollection/index.tsx
@@ -14,99 +14,101 @@ import { Checkbox } from "@chakra-ui/react";
 // import artistWrapper from "../../assets/imgs/artistsWrapper.png";
 
 const ExploreCollection = () => {
-	const conRef: any = useRef();
-	const [collapsed, setCollapsed] = useState(false);
-	const [isAvailable, setIsAvailable] = useState<boolean | null>(null);
-	// eslint-disable-next-line no-unused-vars
-	const [collections, setCollections] = useState<Collection[]>([]);
+  const conRef: any = useRef();
+  const [collapsed, setCollapsed] = useState(false);
+  const [isAvailable, setIsAvailable] = useState<boolean | null>(null);
+  // eslint-disable-next-line no-unused-vars
+  const [collections, setCollections] = useState<Collection[]>([]);
 
-	const { ref }: any = useResizeDetector();
+  const { ref }: any = useResizeDetector();
 
-	const { getAllCollections } = useApi();
+  const { getAllCollections } = useApi();
 
-	useEffect(() => {
-		const updateCollections = async () => {
-			const _collections = await getAllCollections(isAvailable);
-			setCollections(_collections.data);
-		};
+  useEffect(() => {
+    const updateCollections = async () => {
+      const _collections = await getAllCollections(isAvailable);
+      setCollections(_collections.data);
+    };
 
-		updateCollections();
-		// eslint-disable-next-line react-hooks/exhaustive-deps
-	}, [isAvailable]);
+    updateCollections();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isAvailable]);
 
-	const handleAvailable = async (event: any, _isAvailable: boolean) => {
-		if (event.target.checked) {
-			setIsAvailable(_isAvailable);
-		} else {
-			setIsAvailable(null);
-		}
+  const handleAvailable = async (event: any, _isAvailable: boolean) => {
+    if (event.target.checked) {
+      setIsAvailable(_isAvailable);
+    } else {
+      setIsAvailable(null);
+    }
 
-		// console.log(isAvailable);
-	};
+    // console.log(isAvailable);
+  };
 
-	return (
-		<div>
-			<Header />
-			<div
-				ref={conRef}
-				className={styles.container}
-				// onScroll={width <= 600 ? handleScroll : null}
-			>
-				<div className={cx(styles.sidebar, collapsed && styles.collapsed)}>
-					<div className={styles.sidebarHeader}>
-						{!collapsed && <div className={styles.sidebarTitle}>Filter</div>}
-						<img
-							src={iconCollapse}
-							className={styles.iconCollapse}
-							onClick={() => setCollapsed(!collapsed)}
-							alt='collapse-menu'
-						/>
-					</div>
-					<div className={styles.filterList}>
-						<div className={styles.filterList}>
-							<div className={styles.titleFilter}>Availability</div>
-							<Checkbox
-								paddingBottom={"8px"}
-								paddingTop={"8px"}
-								onChange={(e) => handleAvailable(e, true)}>
-								<span className={styles.check}>Available</span>
-							</Checkbox>
-							<Checkbox onChange={(e) => handleAvailable(e, false)}>
-								<span className={styles.check}>Sold Out</span>
-							</Checkbox>
-						</div>
-						<div className={styles.filterList}>
-							<div
-								className={styles.titleFilter}
-								style={{ paddingBottom: "8px" }}>
-								Photographer
-							</div>
-							<div className={styles.filterLists}>Add check boxes</div>
-						</div>
-					</div>
-				</div>
-				<div className={styles.body}>
-					<div
-						ref={ref}
-						className={styles.exploreAll}
-						// onScroll={width > 600 ? handleScroll : null}
-					>
-						{collections.map((collection: Collection) => {
-							return NftItem(collection);
-						})}
-						{/* <CollectionsGrid
+  return (
+    <div>
+      <Header />
+      <div
+        ref={conRef}
+        className={styles.container}
+        // onScroll={width <= 600 ? handleScroll : null}
+      >
+        <div className={cx(styles.sidebar, collapsed && styles.collapsed)}>
+          <div className={styles.sidebarHeader}>
+            {!collapsed && <div className={styles.sidebarTitle}>Filter</div>}
+            <img
+              src={iconCollapse}
+              className={styles.iconCollapse}
+              onClick={() => setCollapsed(!collapsed)}
+              alt="collapse-menu"
+            />
+          </div>
+          <div className={styles.filterList}>
+            <div className={styles.filterList}>
+              <div className={styles.titleFilter}>Availability</div>
+              <Checkbox
+                paddingBottom={"8px"}
+                paddingTop={"8px"}
+                onChange={(e) => handleAvailable(e, true)}
+              >
+                <span className={styles.check}>Available</span>
+              </Checkbox>
+              <Checkbox onChange={(e) => handleAvailable(e, false)}>
+                <span className={styles.check}>Sold Out</span>
+              </Checkbox>
+            </div>
+            <div className={styles.filterList}>
+              <div
+                className={styles.titleFilter}
+                style={{ paddingBottom: "8px" }}
+              >
+                Photographer
+              </div>
+              <div className={styles.filterLists}>Add check boxes</div>
+            </div>
+          </div>
+        </div>
+        <div className={styles.body}>
+          <div
+            ref={ref}
+            className={styles.exploreAll}
+            // onScroll={width > 600 ? handleScroll : null}
+          >
+            {collections.map((collection: Collection) => {
+              return NftItem(collection, false);
+            })}
+            {/* <CollectionsGrid
 							items={collections}
 							uploading={upFetching}
 							loading={downFetching}
 							numPerRow={numPerRow}
 							category={category}
 						/> */}
-					</div>
-				</div>
-			</div>
-			<Footer />
-		</div>
-	);
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default ExploreCollection;
