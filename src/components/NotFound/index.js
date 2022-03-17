1494b1f9f5e685247184388f666a394cb0e364c0
Matteo Georges <matteo.georges74@hotmail.fr>
2022-03-17 22:07:10 +0100

    fix: 404 page

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -8,35 +8,35 @@ import styles from "./styles.module.scss";
 import Header from "components/Header";
 
 const NotFound = () => {
-	// const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-	// useEffect(() => {
-	// 	dispatch(HeaderActions.toggleSearchbar(false));
-	// 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	// }, []);
+  // useEffect(() => {
+  // 	dispatch(HeaderActions.toggleSearchbar(false));
+  // 	// eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []);
 
-	return (
-		<div className={styles.container}>
-			<Header />
-			<div className={styles.body}>
-				<img
-					src={exposure}
-					alt='exposure'
-					className={styles.man}
-					style={{ filter: "invert(var(--color-logo))" }}
-				/>
-				<div className={styles.main}>
-					<div className={styles.title}>404</div>
-					<div className={styles.subtitle}>
-						We sadly couldn’t find the page you’re looking for :(
-					</div>
-					<Link to='/' className={styles.button}>
-						Back To Home
-					</Link>
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <Header />
+      <div className={styles.body}>
+        <img
+          src={exposure}
+          alt="exposure"
+          className={styles.man}
+          style={{ filter: "invert(var(--color-logo))" }}
+        />
+        <div className={styles.main}>
+          <div className={styles.title}>404</div>
+          <div className={styles.subtitle}>
+            We sadly couldn’t find the page you’re looking for :(
+          </div>
+          <Link to="/" className={styles.button}>
+            Back To Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
