1b9e2ba27f0ca1056fe736e5ac60ffa4c74bf79b
Lorcann <contact@lorcannrauzduel.fr>
2022-05-18 18:19:30 +0200

    Added CDN constants

---
+++ src/constants/cdn.constants.ts
@@ -0,0 +1,35 @@
+export const IMAGE_DELIVERY_URL = "https://imagedelivery.net/Ui4hX-mywdi4eO8Amekoxw";
+
+export const getCDNLink = (id: String) => {
+    console.log(`get CDN link: ${IMAGE_DELIVERY_URL}/${id}/public`)
+    return `${IMAGE_DELIVERY_URL}/${id}/public`;
+}
+
+export const logo = getCDNLink('e7cfb02c-09c9-4231-20e5-07f8fb3bfe00');
+export const logoWhite = getCDNLink('a7b337fb-95ef-4256-b423-3060f7a6e200');
+export const discord = getCDNLink('5c3f501a-8376-42ee-2d9d-ef7bcb1f9700');
+export const instagram = getCDNLink('8e1ea7cb-e3d3-423d-267b-dcd5707dc600');
+export const twitter = getCDNLink('30c1acee-dc81-4435-51f1-ba93e9d63100');
+export const down = getCDNLink('cd459770-13fc-48d1-221a-f8b37fb9a200');
+export const disc = getCDNLink('c1f3daa2-a72f-4a40-5c25-11e35175e600');
+export const profile = getCDNLink('0a1b2917-cebf-424e-04ea-532bee007500');
+export const wallet = getCDNLink('423a84c3-ff81-413a-e1b0-d14dd882ca00');
+export const arr = getCDNLink('2d088f43-000d-42ab-55d1-fcb75aa6e700');
+export const ticon = getCDNLink('c170f6ea-b528-4687-06d1-de51e0469200');
+export const artistWrapper = getCDNLink('203f0b61-8a21-4689-8509-6bafa266cb00');
+export const dropWrapper = getCDNLink('bc9161b7-75b2-4556-ccff-4e2cc2d9cb00');
+export const copyRights = getCDNLink('4100f097-ebd2-4e29-d8d6-afe6064f6700');
+export const mintType = getCDNLink('224cc1f6-07f3-4d22-2e7f-98c24f37c500');
+export const agenda = getCDNLink('dadbd942-6285-4a17-8b3f-db11d79c5500');
+export const available = getCDNLink('6a9a41e3-683d-4c06-c9d0-7b601ddf6f00');
+export const ether = getCDNLink('37cabedf-dc59-40ba-92c8-1fc4fae7fd00');
+export const borderbl = getCDNLink('0a544d1a-d1cd-44f4-4516-2f2cf3175a00');
+export const borderbr = getCDNLink('6945d31f-c02b-4d6c-2ccb-f13e62d68200');
+export const borderml = getCDNLink('5e715c3f-e7da-4e64-bd6c-c1be54dc8700');
+export const bordermr = getCDNLink('093d196e-ca95-4688-6818-df64df4f3200');
+export const bordertl = getCDNLink('76b4fc76-93b2-4fca-5dd7-a911c8ba0400');
+export const bordertr = getCDNLink('1d6eca38-7ee0-4df1-3b9c-c7818e4a7600');
+export const largeGrid = getCDNLink('de76eb90-a2dc-408d-0d10-745e58780f00');
+export const smallGrid = getCDNLink('0a650aa8-2a92-4d58-bab6-d995717d9d00');
+export const addImage = getCDNLink('9b9bc565-afb6-4b67-d477-4143de120200');
+export const arrow = getCDNLink('cecf904b-a605-465e-55f8-1af0f7785600');
